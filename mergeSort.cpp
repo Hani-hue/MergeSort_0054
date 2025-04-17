96246f253d584e00096462302c2b9002458cae49
Hani-hue <isnahabibah8729@gmail.com>
2025-04-17 10:33:36 +0700

    perulangan for

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -1,2 +1,31 @@
 #include <iostream>
 using namespace std;
+
+int arr[20], B[20];
+
+int n;
+
+void input()
+{
+    cout << "Masukkan Panjang element array; ";
+    cin >> n;
+
+    if (n <= 20)
+    {
+        break;
+    }
+    else
+    {
+        cout << "\nMaksimal panjang array adalah 20";
+    }
+
+    cout << "\n==========================================";
+    cout << "n\Inputkan Isi Element Array";
+    cout << "\n==========================================" << endl;
+
+    for (INT I = 0; i < n; i++)
+    {
+        cout << "Array index ke -  " << i << ": ";
+        cin >> arr[i]
+    }
+}
